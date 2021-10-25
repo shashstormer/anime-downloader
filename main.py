d7c6a7b0f137b9ee3a1d11392db7b97b3f97e6b9
shash <shashanka5398@gmail.com>
2021-10-25 10:55:02 +0530

    downloader updated

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ downloader made on 23-9-21
 from requirements import inner_main, config
 
 if config.anime_folder == "c:/users/user/downloads/anime":
-    print("please set up the config.py file located in requirements folder")
+    print("please set the variable anime_folder in config.py file located in requirements folder")
     input("press enter to exit...")
 else:
     inner_main.other(config.program_name).name()
-    inner_main.main().full_function(input("enter anime name : "))
+    try:
+        inner_main.main().full_function(input("enter anime name : "))
+    except Exception as e:
+        print(e)
