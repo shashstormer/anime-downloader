17543c6241de57abd74c56190b76c1feb88b3e40
shash <shashanka5398@gmail.com>
2021-10-25 11:07:13 +0530

    downloader updated

--- requirements/config.py
+++ requirements/config.py
@@ -5,5 +5,4 @@ config files
 # suggested to create an empty folder to store the downloaded anime
 # also don't put a \ or / in the end of the folder name here
 anime_folder = fr"c:/users/user/downloads/anime"
-
 program_name = "anime downloader"
