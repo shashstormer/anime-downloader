35480ab3884a88df2c5e2aef7817a8735e5381c3
shash <shashanka5398@gmail.com>
2021-11-05 21:20:37 +0530

    downloader updated

--- requirements/get_video_ids.py
+++ requirements/get_video_ids.py
@@ -16,18 +16,17 @@ function to get video ids from a gogoanime page
     episodes = anime_objet[list(anime_objet.keys())[0]]["episode urls"]
     episodes_list = list(episodes.keys())
     for key in episodes_list:
-        print(f"scrapping links for episode {key}")
+        print(f"getting download links for episode {key}")
         ep_url = episodes[key].get("episode url")
         ep_page = page_process(ep_url)
         ep_page = str(ep_page)
         ep_page = ep_page.split("\n")
         for line in ep_page:
-            if "data-video=\"//goload.one/streaming.php?" in line:
+            if "https://gogoplay1.com/download?id=" in line:
                 # print(line)
-                line = line.split("data-video=\"")[1].split("\"")[0].replace("streaming.php", "download")
-                # print(line)
+                line = line.split("href=\"")[1].split("\"")[0]
                 line = episodes[key]["goload"] = f"https:{line}"
-                # print(f"{line}")
+                print(f"{line}")
 
             if "<a data-video=\"https://" in line:
                 line = line.split("\"")[1]
@@ -36,7 +35,7 @@ function to get video ids from a gogoanime page
                     line = line.split("/")[0]
                     line = episodes[key]["streamtape"] = f"https://streamtape.com/v/{line}"
                 if "sbplay" in line:
-                    print(line)
+                    # print(line)
                     line = line.split(".html")[0]
                     line = line.split("e/")[1]
                     line = episodes[key]["sbplay"] = f"https://tubesb.com/d/{line}"
@@ -46,7 +45,8 @@ function to get video ids from a gogoanime page
                 if "embedsito" in line:
                     line = line.replace("/v/", "/f/")
                     line = episodes[key]["xstream"] = f"{line}"
-                print(line)
+                if "dood.la/" in line:
+                    print(line)
     anime_objet[list(anime_objet.keys())[0]]["episode urls"] = episodes
     return anime_objet
 
