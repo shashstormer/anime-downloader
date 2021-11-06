8d6bc255ff4f4791d229d0aa42b9d759c45c0576
shash <shashanka5398@gmail.com>
2021-11-06 18:18:41 +0530

    downloader updated

--- requirements/scrappers.py
+++ requirements/scrappers.py
@@ -1,13 +1,12 @@
 """
 call all scrappers
 """
-from requirements.dodostream import dodostream as dodostream
 from requirements.streamsb import stream_sb as stream_sb
 from requirements.get_episodes_list import get_episodes_list_and_urls as \
     get_episodes_list_and_urls
 from requirements.search_gogo_anime_term import search_ as search_
 from requirements.get_video_ids import get_video_ids as get_video_ids
-from requirements.goload import goload as goload
+# from requirements.goload import goload as goload
 # from requirements.streamtape import streamtape as streamtape
 
 
@@ -20,13 +19,6 @@ def scrape(anime_object):
     for key in anime_object[list(anime_object.keys())[0]]["episode urls"]:
         anime_object[list(anime_object.keys())[0]]["episode urls"][key]["downloadable"] = []
     anime_objet = anime_object
-    try:
-        anime_object = goload(anime_object)
-        anime_objet = anime_object
-    except Exception as e:
-        if e != e:
-            print(e)
-        anime_object = anime_objet
     try:
         anime_object = stream_sb(anime_object)
         anime_objet = anime_object
